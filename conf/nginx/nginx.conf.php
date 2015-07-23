91cf913e58a7fdbc41b9565d79e785bdbc71de2e
Xev <sevjij@gmail.com>
2015-07-24 01:00:32 +0200

    fix of nginx.conf.php

--- conf/nginx/nginx.conf.php
+++ conf/nginx/nginx.conf.php
@@ -1,12 +1,4 @@
 #user  nobody;
-user nginx;
-worker_processes  auto;
-
-error_log  stderr;
-#error_log  logs/error.log  notice;
-#error_log  logs/error.log  info;
-
-#user  nobody;
 worker_processes  auto;
 
 error_log  stderr;
@@ -14,33 +6,7 @@ error_log  stderr;
 #error_log  logs/error.log  info;
 
 #pid        logs/nginx.pid;
-pid         <?=getenv('PID_PATH')?>;
-
-
-events {
-worker_connections  1024;
-}
-
-
-http {
-include       mime.types;
-default_type  application/octet-stream;
-
-#log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-#                  '$status $body_bytes_sent "$http_referer" '
-#                  '"$http_user_agent" "$http_x_forwarded_for"';
-
-#access_log  logs/access.log  main;
-
-sendfile        on;
-#tcp_nopush     on;
-
-#keepalive_timeout  0;
-keepalive_timeout  65;
-
-#gzip  on;
-}
-#pid        logs/nginx.pid;
+pid        <?=getenv('PID_PATH')?>;
 
 
 events {
@@ -66,3 +32,4 @@ http {
 
     #gzip  on;
 }
+
