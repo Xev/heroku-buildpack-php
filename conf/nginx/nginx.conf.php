a4d283b274e6f67169dce38a41c5f3c6df797cb8
Xev <sevjij@gmail.com>
2015-07-24 00:54:54 +0200

    generate nginx main config, and use pid path

--- conf/nginx/nginx.conf.php
+++ conf/nginx/nginx.conf.php
@@ -1,4 +1,3 @@
-
 #user  nobody;
 user nginx;
 worker_processes  auto;
@@ -7,6 +6,40 @@ error_log  stderr;
 #error_log  logs/error.log  notice;
 #error_log  logs/error.log  info;
 
+#user  nobody;
+worker_processes  auto;
+
+error_log  stderr;
+#error_log  logs/error.log  notice;
+#error_log  logs/error.log  info;
+
+#pid        logs/nginx.pid;
+pid         <?=getenv('PID_PATH')?>;
+
+
+events {
+worker_connections  1024;
+}
+
+
+http {
+include       mime.types;
+default_type  application/octet-stream;
+
+#log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+#                  '$status $body_bytes_sent "$http_referer" '
+#                  '"$http_user_agent" "$http_x_forwarded_for"';
+
+#access_log  logs/access.log  main;
+
+sendfile        on;
+#tcp_nopush     on;
+
+#keepalive_timeout  0;
+keepalive_timeout  65;
+
+#gzip  on;
+}
 #pid        logs/nginx.pid;
 
 
