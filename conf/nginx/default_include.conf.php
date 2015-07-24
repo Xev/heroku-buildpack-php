6d363d6dc88c8a765ff40054a0734757f556aa95
Xev <sevjij@gmail.com>
2015-07-24 09:54:08 +0200

    Debugbar fix, trailing slash remove, static files cache

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,5 +1,14 @@
 location / {
     index  index.php index.html index.htm;
+    try_files $uri $uri/ /index.php?$query_string;
+}
+
+if (!-d $request_filename) {
+    rewrite     ^/(.+)/$ /$1 permanent;
+}
+
+location ~* \.(?:ico|css|js|jpe?g|JPG|png|svg|woff)$ {
+    expires 365d;
 }
 
 # for people with app root as doc root, restrict access to a few things
